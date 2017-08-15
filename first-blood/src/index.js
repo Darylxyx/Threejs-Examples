ba639d0a62bd4a9788193eafda0600eda2b6567b
Darylxyx <921218060@qq.com>
2017-08-15 16:23:32 +0800

    update

--- first-blood/src/index.js
+++ first-blood/src/index.js
@@ -3,12 +3,42 @@ import './main.css';
 
 // import THREE from 'three/build/three.js';
 
-console.log(THREE);
+window.onload = function() {
 
-var canvas = document.querySelector('.container');
+	var container = document.querySelector('.container'),
+		conW = container.offsetWidth,
+		conH = container.offsetHeight;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
-canvas.appendChild(renderer.domElement);
+	// var renderer = new THREE.WebGLRenderer({antialias: true});
+	// renderer.setSize(conW, conH);
+	// container.appendChild(renderer.domElement);
 
-console.log(renderer);
+	// var scene = new THREE.Scene();
+
+	// var camera = new THREE.PerspectiveCamera(45, conW/conH, 1, 4000);
+
+	// 创建渲染器
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(conW, conH);
+	container.appendChild(renderer.domElement);
+
+	// 创建场景
+	var scene = new THREE.Scene();
+
+	// 创建相机，并添加到场景中
+	var camera = new THREE.PerspectiveCamera(45, conW/conH, 1, 4000);
+	camera.position.set(0, 0, 3.3333);
+	scene.add(camera);
+
+	// 创建几何体，并添加到场景中
+	var geometry = new THREE.PlaneGeometry(1, 1),
+		mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial());
+	scene.add(mesh);
+	console.log(geometry);
+
+
+	// 渲染绘制
+	renderer.render(scene, camera);
+};
+
+
