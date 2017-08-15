9f0d919cc5468f245fee1dedf4963fe411ae5866
Darylxyx <921218060@qq.com>
2017-08-15 15:31:53 +0800

    update

--- first-blood/src/index.js
+++ first-blood/src/index.js
@@ -1,6 +1,14 @@
 import './base.css';
 import './main.css';
 
-import Three from 'three';
+// import THREE from 'three/build/three.js';
 
-console.log(Three);
+console.log(THREE);
+
+var canvas = document.querySelector('.container');
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+canvas.appendChild(renderer.domElement);
+
+console.log(renderer);
