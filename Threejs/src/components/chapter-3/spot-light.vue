0f312c7179019c865e6a12ed7a1aeb40c57bb5d9
Darylxyx <921218060@qq.com>
2018-07-15 23:35:26 +0800

    update

---
+++ Threejs/src/components/chapter-3/spot-light.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>光源</h1>
+</template>
+<script>
+import mixin from '../../mixins/index';
+
+export default {
+    mixins: [mixin],
+    mounted() {
+        console.log(this.initLight('point'));
+    },
+};
+</script>
